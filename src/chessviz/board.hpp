51d0aa87392cea4aa7c324a3ade2467e792aa37d
Okrushko Nikita <78684182+T1paga@users.noreply.github.com>
2021-05-06 15:46:01 +0700

    Update board.hpp

--- src/chessviz/board.hpp
+++ src/chessviz/board.hpp
@@ -1,18 +1,14 @@
 #ifndef BOARD_HPP
 #define BOARD_HPP
-
 #include <iomanip>
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 using namespace std;
-
 void ChessField(char pole[][9]);  
 void Move(char pole[][9], int* team);
 int MoveCheck(int team, int i1, int i2, int j1, int j2, char pole[][9]);
 int FriendlyFire(int side, int i2, int j2, char pole[][9]);
 int AttackArea(int team, int area, int i2, int j2, char pole[][9]);
-
 #endif
