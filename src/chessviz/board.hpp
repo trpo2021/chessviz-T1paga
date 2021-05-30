13838f5b1432a107c9a7cb48121816e25691776b
T1paga <T1paga>
2021-05-30 18:09:59 +0700

    fix

--- src/chessviz/board.hpp
+++ src/chessviz/board.hpp
@@ -7,5 +7,5 @@
 int FriendlyFire(int side, int i2, int j2, char pole[][9]);
 int AttackArea(int team, int area, int i2, int j2, char pole[][9]);
 int MoveCheck(int team, int i1, int i2, int j1, int j2, char pole[][9]);
-void Chess_Field(char pole[][9]);  
+void Chess_Field(char pole[][9]);
 void Move(char pole[][9], int* team);
